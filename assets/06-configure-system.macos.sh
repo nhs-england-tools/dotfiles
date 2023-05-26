6b2a0d9906634825211adf08a98e95fe744d357f
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2023-05-26 09:38:01 +0200

    Add system configuration script

---
+++ assets/06-configure-system.macos.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+# Configure system macOS
+#
+# Usage:
+#   $ source ./commons.sh
+#   $ ./06-configure-system.macos.sh
+#
+# Options:
+#   VERBOSE=true  # Show all the executed commands, default is `false`
+
+# ==============================================================================
+
+function main {
+
+  configure
+}
+
+function configure {
+
+  defaults write -g com.apple.trackpad.scaling -int 3
+  defaults write -g InitialKeyRepeat -int 15
+  defaults write -g KeyRepeat -int 2
+}
+
+# ==============================================================================
+
+is-arg-true "$VERBOSE" && set -x
+
+main $*
+
+exit 0
---
+++ assets/06-configure-system.ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# Configure system Ubuntu
+#
+# Usage:
+#   $ source ./commons.sh
+#   $ ./06-configure-system.ubuntu.sh
+#
+# Options:
+#   VERBOSE=true  # Show all the executed commands, default is `false`
+
+# ==============================================================================
+
+function main {
+
+  configure
+}
+
+function configure {
+
+  :
+}
+
+# ==============================================================================
+
+is-arg-true "$VERBOSE" && set -x
+
+main $*
+
+exit 0
